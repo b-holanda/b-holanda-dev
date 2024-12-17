4f6cd5a0615e0e5b1375e5ad8c2f0d98eaa42399
Bruno R. holanda <brunorholanda@gmail.com>
2024-12-17 14:38:22 -0300

    opa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import { createSSRApp } from 'vue'
 import App from './App.vue'
+import { createRouter } from './router'
 
-createApp(App).mount('#app')
+import './style.css'
+
+// SSR requires a fresh app instance per request, therefore we export a function
+// that creates a fresh app instance. If using Vuex, we'd also be creating a
+// fresh store here.
+export function createApp() {
+  const app = createSSRApp(App)
+  const pinia = createPinia()
+  app.use(pinia)
+  const router = createRouter()
+  app.use(router)
+  return { app, router }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vuePlugin from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/'
-})
+const base = '/test/'
+
+// preserve this to test loading __filename & __dirname in ESM as Vite polyfills them.
+// if Vite incorrectly load this file, node.js would error out.
+globalThis.__vite_test_filename = __filename
+globalThis.__vite_test_dirname = __dirname
+
+export default defineConfig(({ command, ssrBuild, isSsrBuild }) => ({
+  base,
+  plugins: [
+    vuePlugin(),
+  ],
+  experimental: {
+    renderBuiltUrl(filename, { hostType, type, ssr }) {
+      if (ssr && type === 'asset' && hostType === 'js') {
+        return {
+          runtime: `__ssr_vue_processAssetPath(${JSON.stringify(filename)})`,
+        }
+      }
+    },
+  },
+  build: {
+    minify: false,
+  },
+  ssr: {
+    noExternal: [],
+  },
+  optimizeDeps: {
+    exclude: [],
+  },
+}))
