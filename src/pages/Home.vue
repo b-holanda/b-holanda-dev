08541843fa880ba2ce72e08820a9af8596f1afb1
Bruno R. holanda <brunorholanda@gmail.com>
2024-12-19 01:32:58 -0300

    Add social media links and fix typo in job title on Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,27 @@ import Appbar from '../components/Appbar.vue';
 import Avatar from '../components/Avatar.vue';
 import Sidebar from '../components/Sidebar.vue';
 import avatarImage from '../assets/avatar.png';
+import { ref } from 'vue';
+import { Facebook, Github, Instagram, Linkedin } from 'lucide-vue-next';
 
+const social = ref([
+  {
+    icon: Instagram,
+    url: 'https://www.instagram.com/brunorodriguesholanda/',
+  },
+  {
+    icon: Facebook,
+    url: 'https://www.facebook.com/profile.php?id=100003259137988/',
+  },
+  {
+    icon: Linkedin,
+    url: 'https://www.linkedin.com/in/bruno-r-holanda/',
+  },
+  {
+    icon: Github,
+    url: 'https://github.com/b-holanda/',
+  },
+])
 </script>
 
 <template>
@@ -18,8 +38,15 @@ import avatarImage from '../assets/avatar.png';
         </div>
         <h2 class="text-4xl font-bold mt-4">Bruno Holanda</h2>
         <div class="flex">
-          <h3 class="mr-1">I'm a</h3><span class="animate-pulse">Software Engenner</span>
+          <h3 class="mr-1">I'm a</h3><span class="animate-pulse">Software Engineer</span>
         </div>
+        <ul class="flex w-full justify-center mt-8">
+            <li v-for="(item, index) in social" :key="index" :class="index !== 0 ? 'ml-8' : ''">
+              <a :href="item.url" target="_blank">
+                <component :is="item.icon" />
+              </a>
+            </li>
+          </ul>
       </section>
       <section id="about">
         <article>
